edcffc82191e807a0d7d4a59a2f137c70ebd443e
tkoneru <70915581+tkoneru@users.noreply.github.com>
2023-03-30 23:44:17 -0400

    Updated for Lab 9

--- src/main/java/edu/iu/c322/customerservice/controller/CustomerController.java
+++ src/main/java/edu/iu/c322/customerservice/controller/CustomerController.java
@@ -2,46 +2,57 @@ package edu.iu.c322.customerservice.controller;
 
 import edu.iu.c322.customerservice.model.Customer;
 import edu.iu.c322.customerservice.repository.CustomerRepository;
-import jakarta.validation.Valid;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customers")
-@Repository
-public class CustomerController
-{
+public class CustomerController {
+
     private CustomerRepository repository;
 
-    public CustomerController(CustomerRepository repository)
-    {
+    public CustomerController(CustomerRepository repository) {
         this.repository = repository;
     }
+
+    // Get https:localhost:8080/customers
     @GetMapping
-    // set localhost:8080/customers
-    public List<Customer> findAll()
-    {
+    public List<Customer> findAll(){
         return repository.findAll();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public int create(@Valid @RequestBody Customer customer)
-    {
-        return repository.create(customer);
+    public int create(@Valid @RequestBody Customer customer){
+        Customer addedCustomer = repository.save(customer);
+        return addedCustomer.getId();
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void update(@Valid @RequestBody Customer customer, @PathVariable int id)
-    {
-        repository.update(customer, id);
+    public void update(@Valid @RequestBody Customer customer, @PathVariable int id){
+        customer.setId(id);
+        repository.save(customer);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id)
-    {
-        repository.delete(id);
+    public void delete(@PathVariable int id){
+        Customer customer = new Customer();
+        customer.setId(id);
+        repository.delete(customer);
     }
+   
+    
 }
